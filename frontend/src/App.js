42c4fc049cb0dbf3f1040aa98602000a37d079cc
Trifee <trifee@yahoo.com>
2022-06-03 10:45:18 -0400

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazon trifs</a>
+      </header>
+      <main>Trifee's list of products</main>
+    </div>
+  );
+}
+
+export default App;
